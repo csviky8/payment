d544f18c659c6e6a8e93ab97a7bf1f08ecc08110
Vignesh R <vignesh.r@metaweb.net.au>
2026-07-21 15:43:30 +0530

    Add homepage route for Vercel

--- server.js
+++ server.js
@@ -54,6 +54,14 @@ app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.get("/", (_req, res) => {
+  res.sendFile(require("node:path").join(__dirname, "index.html"));
+});
+
+app.get("/success.html", (_req, res) => {
+  res.sendFile(require("node:path").join(__dirname, "success.html"));
+});
+
 function findPlan(planId) {
   return plans.find((plan) => plan.id === planId);
 }
